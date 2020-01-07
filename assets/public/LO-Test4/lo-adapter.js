d49cab6946d7c567b0505e6d9833f8de34962a42
Shipra bathla <shipra.bathla@comprotechnologies.com>
2020-01-07 17:17:35 +0530

    Add files via upload

--- assets/public/LO-Test4/lo-adapter.js
+++ assets/public/LO-Test4/lo-adapter.js
@@ -4,15 +4,8 @@
  * -------------------
  *
  * Name: Generic Adapter Module
- * Description: Communicates with container, loads engines that expose the following functions:
- *      {
- *          init(),
- *          checkAnswer(),
- *          sendScores(),
- *          reviewAnswers(),
- *          closeLO() 
- *      }
- * 
+ * Description: Communicates with container, loads and interactes with Leonardodls SDK Activity
+ *
  *
  * External Dependencies : ->
  * Following are shared/common dependencies (specified in index.html), and assumed to loaded via the platform)
@@ -28,27 +21,23 @@
 	var activity = null;
 
 	// var to hold current state
-	var currentState = null;
+	var activityState = null;
 
-	// var to hold the current status of LO
-	var currentStatus = null;
+	//var to hold the CheckAnswer button status
+	var checkAnswerBtnVisible = false;
+
+	//var to hold the Hint button status
+	var hintBtnVisible = false;
+
+	//var to hold the Next / Previous button status
+	var nextBtnVisible = false;
+	var prevBtnVisible = false;
 
 	// relative path for the item JSON
 	/*var jsonPath = "data/data.json";*/
 
 	// var to hold unique ID for this LO
 	var activityLaunchId;
-
-	// various status which are sent to container / experience app
-	var status = {
-		CHECKED: 'checked',
-		RESET: 'reset',
-		CHANGE: 'change'
-	}
-
-	// initializing currentScore with 0
-	var currentScore = 0;
-
 
 	/* Function to create/initialize jsChannel */
 	var initChannel = function (callback) {
@@ -66,15 +55,9 @@
 	var bindChannel = function (channel) {
 		channel.bind('receiveMessageFromContainer', function (trans, params) {
 			if (params.type === 'checkAnswers') {
-
-				activity.submit();
-				/*changeControlsVisibility('hint', false, 'Hints');*/
-				/*changeControlsVisibility ('checkAnswers', false, "Check My Work");*/
-
-
+				activity.checkAnswer(checkAnswerCallback.bind(this));
 			} else if (params.type === 'sendScores') {
-				//[Review] - Need clarification and purpose of this event
-				//return engine.sendScores(params.type);
+				activity.submit(submitCallback.bind(this));
 			} else if (params.type === 'reviewAnswers') {
 				//[Review] - Need clarification and purpose of this event
 				//return engine.reviewAnswers(params.type);
@@ -98,7 +81,7 @@
 				let remainingHintsCount = activity.getCurrentItem().showHints();
 
 				if (remainingHintsCount == 0) {
-					changeControlsVisibility('hint', false, "Hints");
+					updateHintBtnStatus(true, false);
 				}
 
 			} else if (params.type === 'currentScreen') {
@@ -109,6 +92,12 @@
 
 				return activity.getItems().length;
 
+			}  else if (params.type === 'tryAgain') {
+
+				activity.getCurrentItem().reset();
+				updateCheckAnswerBtnStatus();
+				updateHintBtnStatus();
+
 			} else if (params.hasOwnProperty('type')) {
 				throw { error: "method_not_found", message: 'method not found' };
 			} else {
@@ -117,7 +106,57 @@
 		});
 	};
 
-	/* Function called when container calls the close method. This function calls engine closeLO method and notifies the 
+	/**
+	 * This is the callback function invoked on activity.checkAnswer()
+	 */
+	var checkAnswerCallback = function(){
+		updateState();
+		updateCheckAnswerBtnStatus(true, false);
+		updateHintBtnStatus(true, false);
+	};
+
+	/**
+	 * This is the callback function invoked on activity.submit()
+	 *
+	 * @param {*} eventArgs
+	 */
+	var submitCallback = function(eventArgs){
+		let payload = {
+			min: eventArgs.response.score.min,
+			max: eventArgs.response.score.max,
+			raw: eventArgs.response.score.raw,
+			scaled: eventArgs.response.score.scaled
+		}
+		updateState();
+		generateStatement('scored', payload);
+		updateCheckAnswerBtnStatus(true, false);
+		updateHintBtnStatus(true, false);
+		showResult(payload.raw);
+	};
+
+	/**
+	 * Function invoked to show the result screen in the container
+	 * @param {*} score
+	 */
+	var showResult = function(score){
+		var params = {
+			type: 'showResult',
+			data: {
+				score: score,
+				review: true
+			}
+		};
+		channel.call({
+			method: 'sendMessageToContainer',
+			params: params,
+			success: function () { },
+			error: function () {
+				console.log('showResult method error');
+			}
+		});
+	}
+
+	/* Function called when container calls the close method. This function calls engine closeLO method and notifies the
 	   container once LO is properly closed and cleaning operations performed, if any */
 	var closeConnections = function () {
 
@@ -129,7 +168,7 @@
 		});
 
 		/*var callback = function () {
-			
+
 		}
 		return activity.destroy(callback);*/
 	};
@@ -184,7 +223,6 @@
 		});
 	}
 
-
 	/* Function called by the engine to pass controls change data to the container */
 	var changeControlsVisibility = function (control, visible, buttonText) {
 		var params = {
@@ -226,17 +264,13 @@
 	};
 
 	var updateState = function () {
-		currentState = {
-			data: activity.getCurrentItem().getState(),
-			status: currentStatus,
-			score: currentScore
-		};
+		activityState = activity.getState();
 
 		channel.call({
 			method: 'sendMessageToContainer',
 			params: {
 				type: 'newState',
-				data: currentState
+				data: activityState
 			},
 			success: function () { },
 			error: function () {
@@ -262,60 +296,31 @@
 			},
 			events: {
 				launched: function (eventArgs) {
-					if (currentState && currentState.data) {
-						activity.getCurrentItem().setState(currentState.data);
+					if (activityState) {
+						activity.setState(activityState);
 					}
 					DOMReady();
 					firstItemReady();
-
-					changeControlsVisibility('checkAnswers', false, "Check Answer");
-					changeControlsVisibility('checkAnswers', true, "Check Answer");
-
-					let itemCount = activity.getItemCount();
-					
-					changeControlsVisibility('goNext', false, "Next");
-					changeControlsVisibility('goPrev', false, "Previous");
-					if (itemCount > 1) {
-						changeControlsVisibility('goPrev', true, "Previous");
-						changeControlsVisibility('goNext', true, "Next");
-					}
-
-					changeControlsVisibility('hint', false, "Hints");
-					if (activity.getCurrentItem().hasHints()) {
-						changeControlsVisibility('hint', true, "Hints");
-					}
-
+					updateCheckAnswerBtnStatus();
+					updateHintBtnStatus();
+					updateNaviagtionButtons();
 				},
 				change: function (eventArgs) {
-					let previousStatus = currentStatus;
-					currentStatus = status.CHANGE;
 					updateState();
-					if (previousStatus != currentStatus) {
-						changeControlsVisibility('checkAnswers', false, "Check Answer");
-						changeControlsVisibility('checkAnswers', true, "Check Answer");
-					}
+					updateCheckAnswerBtnStatus();
 				},
 				destroy: function () {
-
 					console.log('destroy event');
 					closeConnections();
 				},
-				checkAnswer: function (eventArgs) {
-					currentScore = eventArgs.response.score[activity.getCurrentItemIndex()].score.raw;
-					currentStatus = status.CHECKED;
-					updateState();
-					/*changeControlsVisibility('hint', false, "Hints");*/
-				},
 				reset: function (eventArgs) {
-					currentStatus = status.RESET;
 					updateState();
+					updateHintBtnStatus();
 				},
 				itemNavigation: function (eventArgs) {
-					updatedItem = activity.getItem(eventArgs.currentItem.index);
-					changeControlsVisibility('hint', false, "Hints");
-					if (updatedItem.hasHints() && updatedItem.remainingHints()) {
-						changeControlsVisibility('hint', true, "Hints");
-					}
+					updateCheckAnswerBtnStatus();
+					updateHintBtnStatus();
+					updateNaviagtionButtons();
 				},
 				error: function (eventArgs) {
 					console.log(eventArgs);
@@ -336,18 +341,18 @@
 		}
 
 
-		let baseURL = document.baseURI;
+		//let baseURL = document.baseURI;
 
 		// dataJSON is made available in the window by the index.html
-		var activityItems = dataJSON.map(item => {
+		/* var activityItems = dataJSON.map(item => {
 			if (item.type == "url") {
 				return baseURL + item[item.type];
 			}
 			else {
 				return item[item.type];
 			}
-		});
-		LeonardoApp.Activity.init(activityLaunchId, activityItems, container, options).then((player) => {
+		}); */
+		LeonardoApp.Activity.init(activityLaunchId, activityJson2, container, options).then((player) => {
 			activity = player;
 		});
 
@@ -356,14 +361,13 @@
 		/*fetch(jsonPath)
 			.then(res => res.json())
 			.then(function (itemArr) {
-				
-			});*/
 
+		});*/
 
 	}
 
 	/* Function to generate statement - started/launched/scored etc.*/
-	var generateStatement = function (verb) {
+	var generateStatement = function (verb, payload) {
 		var statement = {
 			verb: {
 				"id": "http://adlnet.gov/expapi/verbs/" + verb,
@@ -378,10 +382,10 @@
 		if (verb === 'scored') {
 			statement.result = {
 				score: {
-					max: maxScore,
-					min: minScore,
-					raw: score,
-					scaled: score / maxScore
+					max: payload.max,
+					min: payload.min,
+					raw: payload.raw,
+					scaled: payload.raw / payload.max
 				}
 			}
 		}
@@ -400,6 +404,82 @@
 		});
 	};
 
+	var updateCheckAnswerBtnStatus = function (forceUpdate, visible) {
+		if (forceUpdate) {
+			changeControlsVisibility('checkAnswers', visible, "Check Answer");
+			checkAnswerBtnVisible = visible;
+		}
+		else {
+			let currentItemStatus = activityState ? activityState.itemBody.questions[activity.getCurrentItemIndex()].outcome.status : "NotStarted";
+			if (currentItemStatus == "InProgress") {
+				if (checkAnswerBtnVisible == false) {
+					changeControlsVisibility('checkAnswers', true, "Check Answer");
+					checkAnswerBtnVisible = true;
+				}
+			}
+			else if (checkAnswerBtnVisible) {
+				changeControlsVisibility('checkAnswers', false, "Check Answer");
+				checkAnswerBtnVisible = false;
+			}
+		}
+	};
+
+	var updateHintBtnStatus = function (forceUpdate, visible) {
+		if (forceUpdate) {
+			changeControlsVisibility('hint', visible, "Hints");
+			hintBtnVisible = visible;
+		}
+		else {
+			if (activity.getCurrentItem().hasHints() && activity.getCurrentItem().remainingHints() != 0) {
+				if (hintBtnVisible == false) {
+					changeControlsVisibility('hint', true, "Hints");
+					hintBtnVisible = true;
+				}
+			}
+			else if (hintBtnVisible) {
+				changeControlsVisibility('hint', false, "Hints");
+				hintBtnVisible = false;
+			}
+		}
+	};
+
+	var updateNaviagtionButtons = function () {
+		/* To be handled by Experience App Test Player*/
+		let itemCount = activity.getItemCount();
+		if (itemCount > 1) {
+			if (activity.getCurrentItemIndex() == 0) {
+				if (nextBtnVisible == false) {
+					changeControlsVisibility('goNext', true, "Next");
+					nextBtnVisible = true;
+				}
+				if (prevBtnVisible == true) {
+					changeControlsVisibility('goPrev', false, "Previous");
+					prevBtnVisible = false;
+				}
+			}
+			else if (activity.getCurrentItemIndex() == itemCount - 1) {
+				if (nextBtnVisible == true) {
+					changeControlsVisibility('goNext', false, "Next");
+					nextBtnVisible = false;
+				}
+				if (prevBtnVisible == false) {
+					changeControlsVisibility('goPrev', true, "Previous");
+					prevBtnVisible = true;
+				}
+			}
+			else {
+				if (nextBtnVisible == false) {
+					changeControlsVisibility('goNext', true, "Next");
+					nextBtnVisible = true;
+				}
+				if (prevBtnVisible == false) {
+					changeControlsVisibility('goPrev', true, "Previous");
+					prevBtnVisible = true;
+				}
+			}
+		}
+	}
+
 	/* DOM Ready event */
 	$(document).ready(function () {
 		initChannel(
@@ -414,25 +494,54 @@
 
 					loWithoutControls = initParams.hasOwnProperty('loWithoutControls') ? initParams.loWithoutControls : true;
 					if (initParams.hasOwnProperty('state') && initParams.state) {
-						currentState = initParams.state;
+						activityState = initParams.state;
 					}
 
 					// setting conumer config based on init params and default consumer key present in the index.html of this LO
-					if(initParams && initParams.consumerConfig){
+					if (initParams && initParams.consumerConfig) {
 						consumerConfig = initParams.consumerConfig;
 					}
-					if(consumerConfig){
-						LeonardoApp.setConfig( consumerConfig );
+
+					if (consumerConfig) {
+						LeonardoApp.setConfigV2(consumerConfig);
 					}
-					
+
 					generateStatement('started');
 
 					launchActivity();
 
-					
-					
 				});
 			});
 	});
 
 })();
+
+/* Sample consumer config
+{
+ "consumerConfig": {
+   "players": {
+     "mcq": {
+       "baseURL": "mcq-item-player-test3/releases/",
+       "js": "0.0.4/libs-mcsr-radio-item-player",
+       "dependencies": [
+         "react",
+         "react-dom",
+         "libs-player-ui-components"
+       ]
+     },
+     "react": {
+       "baseURL": "react/releases/",
+       "js": "16.12.0/react.development"
+     },
+     "react-dom": {
+       "baseURL": "react-dom/releases/",
+       "js": "16.12.0/react-dom.development"
+     },
+     "libs-player-ui-components": {
+       "baseURL": "libs-player-ui-components/releases/",
+       "js": "0.0.1/libs-player-ui-components"
+     }
+   }
+ }
+}
+*/
