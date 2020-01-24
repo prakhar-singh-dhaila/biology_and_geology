e6af16aece4643d613365d069e79629f915c63f1
Jasdeep Singh <jasdeep.singh@comprotechnologies.com>
2020-01-24 17:48:00 +0530

    updating LO-1

--- assets/public/LO-1/data/consumerConfig.js
+++ assets/public/LO-1/data/consumerConfig.js
@@ -8,14 +8,14 @@ var consumerConfig = {
     "modules": {
       "multi-item-activity-player": {
         "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/multi-item-activity-player/releases/",
-        "version": "0.0.14",
-        "js": "0.0.14/multiItemActivityPlayer",
-        "css": "0.0.14/multiItemActivityPlayer"
+        "version": "0.0.15",
+        "js": "0.0.15/multiItemActivityPlayer",
+        "css": "0.0.15/multiItemActivityPlayer"
       },
       "image-label-dnd-player": {
         "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/image-label-dnd-player/releases/",
-        "version": "0.0.8",
-        "js": "0.0.8/libs-imagelabel-dnd-item-player",
+        "version": "0.0.10",
+        "js": "0.0.10/libs-imagelabel-dnd-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -24,8 +24,8 @@ var consumerConfig = {
       },
       "mcq-radio-player": {
         "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/mcq-radio-player/releases/",
-        "version": "0.0.12",
-        "js": "0.0.12/libs-mcsr-radio-item-player",
+        "version": "0.0.13",
+        "js": "0.0.13/libs-mcsr-radio-item-player",
         "dependencies": [
           "react",
           "react-dom",
@@ -44,8 +44,8 @@ var consumerConfig = {
       },
       "libs-player-ui-components": {
         "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/libs-player-ui-components/releases/",
-        "version": "0.0.7",
-        "js": "0.0.7/libs-player-ui-components"
+        "version": "0.0.8",
+        "js": "0.0.8/libs-player-ui-components"
       }
     }
   }
