f65256056194f128a75f6bf05134f45f6d454c6b
udit.maheshwari@gmail.com <Yudi@1298>
2020-02-14 12:41:41 +0530

    adding test folder for testing

--- assets/public/test/data/consumerConfig.js
+++ assets/public/test/data/consumerConfig.js
@@ -1,52 +1,152 @@
-var consumerConfig = {
+
+var consumerConfig ={
   "players": {
-    "playerMap": {
-      "collection": "multi-item-activity-player",
-      "imagelabel-dnd": "image-label-dnd-player",
-      "mcsr-radio": "mcq-radio-player"
-    },
-    "modules": {
-      "multi-item-activity-player": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/multi-item-activity-player/releases/",
-        "version": "0.0.15",
-        "js": "0.0.15/multiItemActivityPlayer",
-        "css": "0.0.15/multiItemActivityPlayer"
+      "playerMap": {
+          "collection": "multi-item-activity-player",
+          "imagelabel-dnd": "image-label-dnd-player",
+          "imagelabel-dnd-1": "image-label-dnd-player-1",
+          "imagelabel-dnd-2": "image-label-dnd-player-2",
+          "imagelabel-dnd-3": "image-label-dnd-player-3",
+          "mcsr-radio": "mcq-radio-player",
+          "mcsr-radio-1": "mcq-radio-player-1",
+          "mcsr-radio-2": "mcq-radio-player-2",
+          "mcsr-block": "mcq-block-player",
+          "mcsr-block-1": "mcq-block-player-1",
+          "mcsr-block-2": "mcq-block-player-2"
       },
-      "image-label-dnd-player": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/image-label-dnd-player/releases/",
-        "version": "0.0.12",
-        "js": "0.0.12/libs-imagelabel-dnd-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components"
-        ]
-      },
-      "mcq-radio-player": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/mcq-radio-player/releases/",
-        "version": "0.0.13",
-        "js": "0.0.13/libs-mcsr-radio-item-player",
-        "dependencies": [
-          "react",
-          "react-dom",
-          "libs-player-ui-components"
-        ]
-      },
-      "react": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react/releases/",
-        "version": "16.12.0",
-        "js": "16.12.0/react.development"
-      },
-      "react-dom": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react-dom/releases/",
-        "version": "16.12.0",
-        "js": "16.12.0/react-dom.development"
-      },
-      "libs-player-ui-components": {
-        "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/libs-player-ui-components/releases/",
-        "version": "0.0.10",
-        "js": "0.0.10/libs-player-ui-components"
+      "modules": {
+          "multi-item-activity-player": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/multi-item-activity-player/releases/",
+              "version": "0.0.1",
+              "js": "0.0.1/multiItemActivityPlayer",
+              "css": "0.0.1/multiItemActivityPlayer",
+              "preloader": "https://d1um2carmvy4c4.cloudfront.net/multi-item-activity-player/releases/0.0.1/miapPreloader"
+          },
+          "image-label-dnd-player": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/dnd-player/",
+              "version": "0.0.11",
+              "js": "libs-imagelabel-dnd-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "image-label-dnd-player-1": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/dnd-player-1/",
+              "version": "0.0.11",
+              "js": "libs-imagelabel-dnd-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "image-label-dnd-player-2": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/dnd-player-2/",
+              "version": "0.0.11",
+              "js": "libs-imagelabel-dnd-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "image-label-dnd-player-3": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/dnd-player-3/",
+              "version": "0.0.11",
+              "js": "libs-imagelabel-dnd-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-radio-player": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/radio-player/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-radio-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-radio-player-1": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/radio-player-1/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-radio-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-radio-player-2": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/radio-player-2/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-radio-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-block-player": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/block-player/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-block-player-1": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/block-player-1/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "mcq-block-player-2": {
+              "baseURL": "https://d1um2carmvy4c4.cloudfront.net/block-player-2/",
+              "version": "0.0.13",
+              "js": "libs-mcsr-item-player",
+              "dependencies": [
+                  "react",
+                  "react-dom",
+                  "libs-player-ui-components"
+              ],
+              "preloader": "DefaultPreloader"
+          },
+          "react": {
+              "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react/releases/",
+              "version": "16.12.0",
+              "js": "16.12.0/react.development"
+          },
+          "react-dom": {
+              "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/react-dom/releases/",
+              "version": "16.12.0",
+              "js": "16.12.0/react-dom.development"
+          },
+          "libs-player-ui-components": {
+              "baseURL": "https://d3f2ws3vg5f2o9.cloudfront.net/modules/libs-player-ui-components/releases/",
+              "version": "0.0.8",
+              "js": "0.0.8/libs-player-ui-components"
+          }
       }
-    }
   }
-}
+};
